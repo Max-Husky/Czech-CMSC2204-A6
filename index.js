cde09a6add6d751411920ad7598a8c6f1df4d3d6
MaxHusky <5143221+Max-Husky@users.noreply.github.com>
2022-11-02 15:14:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 app.listen(
   PORT,
-  () => console.log(`Server Running on port: ${PORT}`);
+  () => console.log(`Server Running on port: ${PORT}`)
 );
 
 // get one user
